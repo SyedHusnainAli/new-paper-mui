dd593151382e7a4b7b2e7f3feaee8f873ad9657d
SyedHusnainAli <syedHusnain1200@gmail.com>
2022-07-26 09:36:04 -0700

    create from material ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import * as React from 'react';
+import NavBar from './NavBar' 
+import Nested from './Nested'
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
+import { withTheme } from '@emotion/react';
+
 
 export default function App() {
   return (
@@ -8,18 +14,42 @@ export default function App() {
       sx={{
         display: 'flex',
         flexWrap: 'wrap',
+        
         '& > :not(style)': {
           m: 1,
-          width: 128,
-          height: 128,
+          background: 'lightBlue',
+          width: 328,
+          height: 558,
+          // background: '#efefef',
+          textAlign: 'center',
+          margin: '0 auto',
+          flexDirection: 'column',
+          
         },
+       
       }}
+
     >
-      <Paper elevation={0} />
-      <Paper />
-      <Paper elevation={3} />
+      <Paper elevation={0}>
+      <NavBar />
+        <h1>Login</h1>
+           <TextField  label="Enter email"  />
+           <br />
+           <br />
+           <TextField  label="Password"  />
+           <br />
+           <br />
+           <Nested />
+           <Button id='btn' variant="outlined">Login</Button>
+          
+      </Paper>
+     
+   
     </Box>
   );
 }
 
 //export default App;
+
+
+// className={classes.paper}
